75bc9926ccb498f0a5f285534dff79eb228dbfd7
Barklim <46284548+Barklim@users.noreply.github.com>
2021-05-25 22:58:44 +0300

    add genomus proj

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -4,6 +4,8 @@ console.log('%c ', 'font-size:600px; line-height:280px; background:url(https://i
 // 2562  x  1329
 // 502  x  813 
 // 460 x 996 for swiper (920 x 1992)
+const noImage = '<div class="swiper-slide"><img class="slideImg" src="assets/img/noImage.webp"/></div>';
+
 const aptekamosTemp1 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/aptekamos/aptekamos1.webp"/></div>';
 const aptekamosTemp2 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/aptekamos/aptekamos2.webp"/></div>';
 const aptekamosTemp3 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/aptekamos/aptekamos3.webp"/></div>';
@@ -18,11 +20,65 @@ const atwintaTemp_md2 = '<div class="swiper-slide"><img class="slideImg" src="as
 const atwintaTemp_sm2 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/atwinta/atwintasm2.webp"/></div>';
 const atwintaTempMoblie_sm2 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/atwinta/atwintasm2.webp"/></div>';
 
+// const genomusTemplater_lg = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomustemplg.webp"/></div>';
+// const genomusTemplater_md = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomustempmd.webp"/></div>';
+// const genomusTemplater_sm = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomustempsm.webp"/></div>';
+const genomusTemplater1 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusTemplater1.webp"/></div>';
+const genomusTemplater2 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusTemplater2.webp"/></div>';
+const genomusTemplater3 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusTemplater3.webp"/></div>';
+const genomusTemplater4 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusTemplater4.webp"/></div>';
+const genomusTemplater5 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusTemplater5.webp"/></div>';
+const genomusTemplater6 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusTemplater6.webp"/></div>';
+const genomusTemplater7 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusTemplater7.webp"/></div>';
+
+// const genomusTemp_lg = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomuslg.webp"/></div>';
+// const genomusTemp_md = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusmd.webp"/></div>';
+// const genomusTemp_sm = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomussm.webp"/></div>';
+const genomusTemp1 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus1.webp"/></div>';
+const genomusTemp2 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus2.webp"/></div>';
+const genomusTemp3 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus3.webp"/></div>';
+const genomusTemp4 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus4.webp"/></div>';
+const genomusTemp5 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus5.webp"/></div>';
+const genomusTemp6 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus6.webp"/></div>';
+const genomusTemp7 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus7.webp"/></div>';
+const genomusTemp8 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus8.webp"/></div>';
+const genomusTemp9 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus9.webp"/></div>';
+const genomusTemp10 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus10.webp"/></div>';
+const genomusTemp11 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomus11.webp"/></div>';
+
+const genomusAdminTemp1 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusAdmin1.webp"/></div>';
+const genomusAdminTemp2 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusAdmin2.webp"/></div>';
+const genomusAdminTemp3 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusAdmin3.webp"/></div>';
+const genomusAdminTemp4 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusAdmin4.webp"/></div>';
+const genomusAdminTemp5 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusAdmin5.webp"/></div>';
+const genomusAdminTemp6 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusAdmin6.webp"/></div>';
+const genomusAdminTemp7 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusAdmin7.webp"/></div>';
+
+const genomusTempMobile1 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile1.webp"/></div>';
+const genomusTempMobile2 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile2.webp"/></div>';
+const genomusTempMobile3 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile3.webp"/></div>';
+const genomusTempMobile4 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile4.webp"/></div>';
+const genomusTempMobile5 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile5.webp"/></div>';
+const genomusTempMobile6 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile6.webp"/></div>';
+const genomusTempMobile7 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile7.webp"/></div>';
+const genomusTempMobile8 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile8.webp"/></div>';
+const genomusTempMobile9 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile9.webp"/></div>';
+const genomusTempMobile10 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile10.webp"/></div>';
+const genomusTempMobile11 = '<div class="swiper-slide"><img class="slideImg" src="assets/img/genomus/genomusMobile11.webp"/></div>';
+
+
 const aptekamosArr = [aptekamosTemp1, aptekamosTemp2, aptekamosTemp3, aptekamosTemp4];
 const atwintaArr1 = [atwintaTemp_lg, atwintaTemp_sm];
 const atwintaMobileArr1 = [atwintaTempMoblie_sm];
 const atwintaArr2 = [atwintaTemp_lg2, atwintaTemp_md2, atwintaTemp_sm2];
 const atwintaMobileArr2 = [atwintaTempMoblie_sm2, atwintaTemp_sm2];
+// const genomusTemplateArr2 = [genomusTemp_lg, genomusTemp_md, genomusTemp_sm];
+const genomusTemplateArr = [genomusTemplater1, genomusTemplater2, genomusTemplater3, genomusTemplater4, genomusTemplater5, genomusTemplater6, genomusTemplater7];
+const genomusTemplateMobileArr = [noImage];
+const genomusArr = [genomusTemp1, genomusTemp2, genomusTemp3, genomusTemp4, genomusTemp5, genomusTemp6, genomusTemp7, genomusTemp8, genomusTemp9, genomusTemp10, genomusTemp11];
+const genomusMobileArr = [genomusTempMobile1, genomusTempMobile2, genomusTempMobile3, genomusTempMobile4, genomusTempMobile5, genomusTempMobile6, genomusTempMobile7, genomusTempMobile8, genomusTempMobile9, genomusTempMobile10];
+const genomusAdminArr = [genomusAdminTemp1, genomusAdminTemp2, genomusAdminTemp3, genomusAdminTemp4, genomusAdminTemp5, genomusAdminTemp6, genomusAdminTemp7];
+const genomusAdminMobileArr = [noImage];
 
 const swiperHeight = document.documentElement.clientHeight - 320;
 let slidesPerViewVar = 1;
@@ -82,11 +138,16 @@ modal1.style.display = "none";
 var scrAptekamos = document.getElementById("screenshots-aptekamos");
 var scrAtwinta = document.getElementById("screenshots-atwinta");
 var scrAtwinta2 = document.getElementById("screenshots-atwinta2");
+var scrGenomusTemplater = document.getElementById("screenshots-genomus-templater");
+var scrGenomus = document.getElementById("screenshots-genomus");
+var scrGenomusAdmin = document.getElementById("screenshots-genomus-admin");
 
 var span = document.getElementsByClassName("close")[0];
 var span1 = document.getElementsByClassName("close1")[0];
 
 scrAptekamos.onclick = function() {
+  swiper.removeAllSlides();
+
   aptekamosArr.forEach(function(item, i, arr) {
     swiper.appendSlide(item);
   });
@@ -113,7 +174,6 @@ scrAtwinta.onclick = function() {
 scrAtwinta2.onclick = function() {
   swiper1.removeAllSlides();
 
-  // isMobile?
   if (slidesPerViewVar === 1)
   {
     atwintaMobileArr2.forEach(function(item, i, arr) {
@@ -121,6 +181,54 @@ scrAtwinta2.onclick = function() {
     });
   } else {
     atwintaArr2.forEach(function(item, i, arr) {
+      swiper1.appendSlide(item);
+    });
+  }
+
+  modal1.style.display = "block";
+}
+scrGenomusTemplater.onclick = function() {
+  swiper1.removeAllSlides();
+
+  if (slidesPerViewVar === 1)
+  {
+    genomusTemplateMobileArr.forEach(function(item, i, arr) {
+      swiper1.appendSlide(item);
+    });
+  } else {
+    genomusTemplateArr.forEach(function(item, i, arr) {
+      swiper1.appendSlide(item);
+    });
+  }
+
+  modal1.style.display = "block";
+}
+scrGenomus.onclick = function() {
+  swiper1.removeAllSlides();
+
+  if (slidesPerViewVar === 1)
+  {
+    genomusMobileArr.forEach(function(item, i, arr) {
+      swiper1.appendSlide(item);
+    });
+  } else {
+    genomusArr.forEach(function(item, i, arr) {
+      swiper1.appendSlide(item);
+    });
+  }
+
+  modal1.style.display = "block";
+}
+scrGenomusAdmin.onclick = function() {
+  swiper1.removeAllSlides();
+
+  if (slidesPerViewVar === 1)
+  {
+    genomusAdminMobileArr.forEach(function(item, i, arr) {
+      swiper1.appendSlide(item);
+    });
+  } else {
+    genomusAdminArr.forEach(function(item, i, arr) {
       swiper1.appendSlide(item);
     });
   }
